58185b14cf78e9876882a9654061d19db2ff1186
MahinourAbdelgawad <mahinourabdelgawad@aucegypt.edu>
2025-05-16 18:35:02 +0300

    added verilog code for smaller modules

---
+++ Verilog Modules/DFF.v
@@ -0,0 +1,13 @@
+module DFF(
+    input clk, d, rst
+    output reg q
+    );
+
+    always @ (posedge clk) begin
+        if (!rst)
+            q <= 1'b0;
+        else
+            q <= d;
+    end
+
+endmodule
